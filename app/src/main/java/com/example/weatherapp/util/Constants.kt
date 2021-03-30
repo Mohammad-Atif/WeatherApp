8a13d5849db7150f4a0bf771040e2bab52951289
Mohammad Atif <mohammadatif92080@gmail.com>
2021-03-30 13:36:42 +0530

    hided the key

--- app/src/main/java/com/example/weatherapp/util/Constants.kt
+++ app/src/main/java/com/example/weatherapp/util/Constants.kt
@@ -4,6 +4,6 @@ class Constants {
 
     companion object{
         const val BASE_URL="https://api.openweathermap.org/"
-        const val API_KEY="47cbc050e9662ed22e30c338957b27d3"
+        const val API_KEY="*******************************"                   //private api key
     }
 }
